98e76512eee7ef44d82894e5d024d8e65c271cc8
Akshay-R <38744637+R0hitNegi@users.noreply.github.com>
2023-08-15 21:29:36 +0530

    LogisticHubApplication

---
+++ src/main/java/com/arh/logistic/LogisticHubApplication.java
@@ -0,0 +1,13 @@
+package com.arh.logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arh/logistic/LogisticHubApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.arh.logistic;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LogisticHubApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
